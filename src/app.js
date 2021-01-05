8d32a32cef5d80a083a0cb5c9718b4e5a81e536e
Rodrigo Demetrio Palma <rodrigo.demetrio.palma@gmail.com>
2021-01-05 15:00:17 -0300

    sentry

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import 'express-async-errors';
 import * as Youch from 'youch';
 import cors from 'cors';
 import helmet from 'helmet';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import routes from './routes';
 import { error } from './app/services/responsePattern';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -13,11 +18,29 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init({
+      dns: sentryConfig.dsn,
+      environment: sentryConfig.environment,
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app: this.server }),
+      ],
+
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 1.0,
+    });
+
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(helmet());
     this.server.use(express.json());
@@ -33,7 +56,7 @@ class App {
 
   routes() {
     this.server.use(routes);
-    // this.server.use(Sentry.Handlers.errorHandler());
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
